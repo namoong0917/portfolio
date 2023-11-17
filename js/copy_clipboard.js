2c0002e2df0e8286cc39dd8d79c835376436d087
namoong0917 <namoong0917@gmail.com>
2023-11-18 03:00:51 +0900

    feat: 토스트삭제 후 클립보드 복사 -> 스크립트 변경

---
+++ js/copy_clipboard.js
@@ -0,0 +1,87 @@
+// 클립보드 복사
+window.onload = function () {
+  function copyToClipboardAndDisplayMessage(copyTarget, message) {
+    const tempInput = document.createElement('input');
+    tempInput.value = copyTarget.textContent;
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    document.execCommand('copy'); // 복사
+    document.body.removeChild(tempInput);
+  
+    if (window.innerWidth > 768) {
+      message.style.display = 'block';
+      message.style.opacity = 1;
+      setTimeout(function () {
+        message.style.opacity = 0;
+      }, 1000);
+      setTimeout(function () {
+        message.style.display = 'none';
+      }, 1300);
+    }
+  }
+
+  function addCopyEventListener(link, copyTarget, message) {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+      copyToClipboardAndDisplayMessage(copyTarget, message);
+    });
+  }
+  
+  // 첫 번째 링크
+  const copyLink1 = document.getElementById('copyLink1');
+  const copyTarget1 = document.getElementById('copyTarget1');
+  const message1 = document.getElementById('message1');
+  addCopyEventListener(copyLink1, copyTarget1, message1);
+
+  // 두 번째 링크
+  const copyLink2 = document.getElementById('copyLink2');
+  const copyTarget2 = document.getElementById('copyTarget2');
+  const message2 = document.getElementById('message2');
+  addCopyEventListener(copyLink2, copyTarget2, message2);
+
+  // 세 번째 링크
+  const copyLink3 = document.getElementById('copyLink3');
+  const copyTarget3 = document.getElementById('copyTarget3');
+  const message3 = document.getElementById('message3');
+  addCopyEventListener(copyLink3, copyTarget3, message3);
+
+  // 네 번째 링크
+  const copyLink4 = document.getElementById('copyLink4');
+  const copyTarget4 = document.getElementById('copyTarget4');
+  const message4 = document.getElementById('message4');
+  addCopyEventListener(copyLink4, copyTarget4, message4);
+};
+// window.onload = function () {
+//   function copyToClipboardAndDisplayMessage(copyTarget, message) {
+//     const tempInput = document.createElement('input');
+//     tempInput.value = copyTarget.textContent;
+//     document.body.appendChild(tempInput);
+//     tempInput.select();
+//     document.execCommand('copy'); // 복사
+//     document.body.removeChild(tempInput);
+
+//     if (window.innerWidth > 768) {
+//       message.style.cssText = 'display: block; opacity: 1;';
+//       setTimeout(function () {
+//         message.style.opacity = 0;
+//       }, 1000);
+//       setTimeout(function () {
+//         message.style.display = 'none';
+//       }, 1300);
+//     }
+//   }
+
+//   function addCopyEventListener(link, copyTarget, message) {
+//     link.addEventListener('click', function (e) {
+//       e.preventDefault();
+//       copyToClipboardAndDisplayMessage(copyTarget, message);
+//     });
+//   }
+
+//   for (let i = 1; i <= 4; i++) {
+//     const copyLink = document.getElementById(`copyLink${i}`);
+//     const copyTarget = document.getElementById(`copyTarget${i}`);
+//     const message = document.getElementById(`message${i}`);
+//     addCopyEventListener(copyLink, copyTarget, message);
+//   }
+// };
